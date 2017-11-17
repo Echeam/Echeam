f8b15ae48769c54714239f5560b6d6490ba853c6
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2017-11-17 10:19:41 +0900

    Modify error

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,34 +1,36 @@
-$(function(){
-  function buildHTML(comment){
-    var html = `<div class = "comment_user_name>
-                <p>${ comment.user_name }</p>
-                </div>
-                <div class = "comment_content>
-                <p>${ comment.content }</p>
-                </div>`
-    return html;
-  }
-    $('#new_comment').on('submit', function(e){
-      e.preventDefault();
-      var formData = new FormData(this);
-      var url = window.location.href + '/comments'
+$(document).on('turbolinks:load', function() {
+  $(function(){
+    function buildHTML(comment){
+      var html = '<div class = comment_user_name>' +
+                 '<p>'+comment.user_name+'</p>' +
+                 '</div>' +
+                 '<div class = comment_content>' +
+                 '<p>'+comment.content+'</p>'
+                 '</div>'
+      return html;
+   }
+      $('#new_comment').on('submit', function(e){
+        e.preventDefault();
+        var formData = new FormData(this);
+        var url = window.location.href + '/comments'
 
-      $.ajax({
-        url: url,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      })
+        $.ajax({
+          url: url,
+          type: "POST",
+          data: formData,
+          dataType: 'json',
+          processData: false,
+          contentType: false
+        })
 
-      .done(function(data){
-        var html = buildHTML(data);
-        $('.proto-comments').append(html)
-        $('#comment-content').val('')
+        .done(function(data){
+          var html = buildHTML(data);
+          $('.proto-comments').append(html)
+          $('#comment-content').val('')
+        })
+        .fail(function(){
+          alert('error');
       })
-      .fail(function(){
-        alert('error');
     })
-  })
+  });
 });
