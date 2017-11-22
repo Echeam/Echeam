bbe3bb0e4fd38074fcdc601712be446a7585eb3c
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2017-11-22 11:00:10 +0900

    Edit like_function

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,16 @@
 class LikesController < ApplicationController
 	def create
-    @like = Like.create(user_id: current_user.id, protptype_id: params[:protptype_id])
-    @likes = Like.where(protptype_id: params[:protptype_id])
-    @protptypes = Protptype.all
+    @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
+    @likes = Like.where(prototype_id: params[:prototype_id])
+    @prototypes = Prototype.all
+    redirect_to prototype_path(params[:prototype_id]), notice: 'いいねしました'
   end
 
   def destroy
-    like = Like.find_by(user_id: current_user.id, protptype_id: params[:protptype_id])
+    like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
     like.destroy
-    @likes = Like.where(protptype_id: params[:protptype_id])
-    @protptypes = Protptype.all
+    @likes = Like.where(prototype_id: params[:prototype_id])
+    @prototypes = Prototype.all
+    redirect_to prototype_path(params[:prototype_id]), notice: 'いいねを削除しました'
   end
 end
