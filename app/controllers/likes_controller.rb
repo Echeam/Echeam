cbeb5d477968fa1076facd4ae8036524b3da789e
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2017-11-22 10:07:20 +0900

    Edit likeaction

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,7 +6,7 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    like = Protptype.find_by(user_id: current_user.id, protptype_id: params[:protptype_id])
+    like = Like.find_by(user_id: current_user.id, protptype_id: params[:protptype_id])
     like.destroy
     @likes = Like.where(protptype_id: params[:protptype_id])
     @protptypes = Protptype.all
