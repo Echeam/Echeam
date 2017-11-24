f7ec6f5b2659b717f3c555806a3391e8464735e5
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2017-11-24 16:52:03 +0900

    Edit prototype model

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -7,6 +7,7 @@ class Prototype < ActiveRecord::Base
   has_many :prototype_tags
 
   accepts_nested_attributes_for :captured_images, reject_if: :reject_sub_images
+  accepts_nested_attributes_for :tags
 
   validates :title,
             :catch_copy,
