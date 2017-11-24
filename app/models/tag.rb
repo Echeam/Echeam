495799aad854a5add599e2bc9c74b7b15e10f6ae
hideokaizuka <hideokaizuka@macuser-no-MacBook-Air-2.local>
2017-11-24 10:27:53 +0900

    Modify model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
-  has_many :protoypes, through: :prototype_tags,
+  has_many :protoypes, through: :prototype_tags
   has_many :prototype_tags
-  validates :name,presence:true,
+  validates :name,presence:true
 end
